97e268f8ec82fd0d579d9d0bac0ec876b8de3be1
uvinaykumar <55868274+uvinaykumar@users.noreply.github.com>
2024-05-11 17:47:08 +0530

    Delete formsproject directory

--- formsproject/formsapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import formsmodel
-
-class AdminName(admin.ModelAdmin):
-    list_display = ['name','email','sal','loc','mobile']
-
-admin.site.register(formsmodel,AdminName)
-
--- formsproject/formsapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FormsappConfig(AppConfig):
-    name = 'formsapp'
